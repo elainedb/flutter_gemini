4f1e60ec1087ac942302709eb89ce0f809d4cf26
elaine <elainedb.dev@gmail.com>
2025-09-15 22:56:43 +0200

    fix tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,26 @@
+
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/di.dart';
+import 'package:flutter_gemini/features/login/presentation/bloc/login_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_gemini/main.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockLoginBloc extends MockBloc<LoginEvent, LoginState> implements LoginBloc {}
 
 void main() {
+  late LoginBloc loginBloc;
+
+  setUp(() {
+    loginBloc = MockLoginBloc();
+    when(() => loginBloc.state).thenReturn(const LoginState.initial());
+    if (getIt.isRegistered<LoginBloc>()) {
+      getIt.unregister<LoginBloc>();
+    }
+    getIt.registerSingleton<LoginBloc>(loginBloc);
+  });
+
   testWidgets('Login screen smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
