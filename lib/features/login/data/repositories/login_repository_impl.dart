daceb8ef2905e965b39d2f6c958dd86998414bbd
elaine <elainedb.dev@gmail.com>
2025-09-15 22:48:04 +0200

    fix

--- lib/features/login/data/repositories/login_repository_impl.dart
+++ lib/features/login/data/repositories/login_repository_impl.dart
@@ -1,12 +1,11 @@
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_gemini/core/config/app_config.dart';
 import 'package:flutter_gemini/core/errors/failures.dart';
 import 'package:flutter_gemini/features/login/data/datasources/login_remote_data_source.dart';
 import 'package:flutter_gemini/features/login/domain/repositories/login_repository.dart';
 import 'package:injectable/injectable.dart';
-
-const authorizedEmailsFromEnv = String.fromEnvironment('AUTHORIZED_EMAILS');
 
 @LazySingleton(as: LoginRepository)
 class LoginRepositoryImpl implements LoginRepository {
@@ -18,8 +17,9 @@ class LoginRepositoryImpl implements LoginRepository {
   Future<Either<Failure, Unit>> signInWithGoogle() async {
     try {
       final email = await remoteDataSource.signInWithGoogle();
-      final authorizedEmails = authorizedEmailsFromEnv.isNotEmpty ? authorizedEmailsFromEnv.split(',') : ['dummy@example.com'];
-      if (authorizedEmails.contains(email)) {
+      final authorizedEmails = await getAuthorizedEmails();
+      debugPrint('Authorized emails: $authorizedEmails');
+      if (authorizedEmails.any((authorizedEmail) => authorizedEmail.trim().toLowerCase() == email.toLowerCase())) {
         debugPrint('Access granted to $email');
         return const Right(unit);
       } else {
